f9badb4b2ea3286807f74fe954a2e99828bf46ca
NSDdet <145782281+NSDdet@users.noreply.github.com>
2024-10-17 10:53:51 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || 'fe052595-b182-4e73-82bf-e470d559ad28'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo123.uioyiguhxdfd.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWZmYWU1NmJkNzA2MmIzMGQ1Y2RiYmRhMjNhNzE0YzQiLCJ0IjoiMzU0M2I1NmItMWNlMS00M2E2LThmOWUtZDEwYjNlNzY0MjU2IiwicyI6Ik9UZzBZVFZrTUdRdE1tTTNaaTAwWmprNExUbGlOR0l0TlRBMFptWmpORGc1TkRaaiJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo1.kensservicecenter.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWZmYWU1NmJkNzA2MmIzMGQ1Y2RiYmRhMjNhNzE0YzQiLCJ0IjoiZmYxZDA0NTctYzViMC00MDRkLTkzOGYtMDVkY2NmNzViMjRjIiwicyI6Ik56ZGpPRGs0WWpRdE9USmxOUzAwTnpNMkxUbGpZelF0T0dJeE9XSmlPR1E1Tm1JeCJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
